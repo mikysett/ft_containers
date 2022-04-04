ba3cc6ac9fd4021952497d37987ab833979b84f9
Michele Sessa <mikysett@gmail.com>
2022-04-04 20:10:52 +0100

    bug fixes on make_pair and iterator_traits

--- headers/iterator_traits.hpp
+++ headers/iterator_traits.hpp
@@ -6,20 +6,13 @@
 
 namespace ft
 {
-	// struct input_iterator_tag { };
-	// struct output_iterator_tag { };
-	// struct forward_iterator_tag : public input_iterator_tag { };
-	// struct bidirectional_iterator_tag : public forward_iterator_tag { };
-	// struct random_access_iterator_tag : public bidirectional_iterator_tag { };
-	// struct contiguous_iterator_tag: public random_access_iterator_tag { };
-
 	template< class Iter >
 	struct iterator_traits {
-		typedef typename Iter::difference_type;
-		typedef typename Iter::value_type;
-		typedef typename Iter::pointer;
-		typedef typename Iter::reference;
-		typedef typename Iter::iterator_category;
+		typedef typename Iter::difference_type difference_type;
+		typedef typename Iter::value_type value_type;
+		typedef typename Iter::pointer pointer;
+		typedef typename Iter::reference reference;
+		typedef typename Iter::iterator_category iterator_category;
 	};
 
 	template< class T >
